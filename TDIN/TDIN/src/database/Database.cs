05e70b38ff04ba65d5582dcfba9ef518ef9ad55a
Nuno Martins <nunojorge90@hotmail.com>
2018-03-17 14:49:09 +0000

    ...

--- TDIN/TDIN/src/database/Database.cs
+++ TDIN/TDIN/src/database/Database.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+using System.Data.Common;
 
 namespace TDIN.src.database
 {
     class Database
     {
+        static void createDatabase()
+        {
+            
+        }
     }
 }
