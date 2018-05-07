27cff48ae352d5e431886233b9cd97aaf791b6c5
Nuno Martins <nunojorge90@hotmail.com>
2018-05-07 20:26:40 +0100

    proj sln

--- proj2/proj2/Program.cs
+++ proj2/proj2/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace proj2
+namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press ENTER to exit ...");
+            Console.ReadLine();
         }
     }
 }
